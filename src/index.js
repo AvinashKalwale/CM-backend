6cb2d027e67d17c00c6686dd9fa17fdfd76acbcc
Avinash Kalwale <avinashkalwale12@gmail.com>
2023-01-12 12:47:49 +0530

    get post delete functions added

--- src/index.js
+++ src/index.js
@@ -20,6 +20,62 @@ app.use(
   })
 );
 
+app.post("/contacts", async (req, res) => {
+    let updated = req.body.data;
+    let id = req.body.userId;
+    for (let i = 0; i < updated.length; i++) {
+      let objUpdate = updated[i];
+      objUpdate.UserId = id;
+      updated[i] = objUpdate;
+    }
+    await contact.insertMany(updated, function (err, datas) {
+      if (err) {
+        console.log(err);
+      }
+      res.send(JSON.stringify({ message: "sucessfully saved", data: datas }));
+    });
+  });
+  
+  app.get("/contacts/:id", async (req, res) => {
+    let paramId = req.params["id"];
+    const strid = paramId.valueOf();
+    await contact
+      .find({ UserId: strid }, function (err, datas) {
+        if (err) {
+          console.log(err);
+        }
+        res.send(JSON.stringify({ message: "sucessfully saved", data: datas }));
+      }).clone();
+  });
+  
+  app.delete("/contacts", async (req, res) => {
+    let deleteIdArray = req.body;
+    deleteIdArray = deleteIdArray.map((x) => mongoose.Types.ObjectId(x));
+    await contact
+      .deleteMany({ _id: { $in: deleteIdArray } }, function (err, delCount) {
+        if (err) {
+          console.log(err);
+        }
+        res.send(
+          JSON.stringify({ message: "sucessfully deletes", data: delCount })
+        );
+      }).clone();
+  });
+  
+  //this is now i used for postman deleteall ...
+  app.delete("/emptycontacts", async (req, res) => {
+    //temporary api call for delete // by putting if req.body,we can make one api call for delete and delerte all..
+    await contact
+      .deleteMany({}, function (err, delCount) {
+        if (err) {
+          console.log(err);
+        }
+        res.send(
+          JSON.stringify({ message: "sucessfully deleted all", data: delCount })
+        );
+      }).clone();
+  });
+
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Example app listening at http://localhost:${port}`);
