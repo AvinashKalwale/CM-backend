23c24655d3a01d67db6121b76418b1cb93d21da9
Avinash Kalwale <avinashkalwale12@gmail.com>
2023-01-13 11:26:32 +0530

    get and post method added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -21,3 +21,84 @@ router.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 router.use(bodyParser.json());
 router.use("/user", userAuthentication);
+
+router.post("/register", async (req, res) => {
+    try {
+      const {email, password } = req.body;
+      
+      const salt = await bcrypt.genSalt(10);
+      const hashPassword = await bcrypt.hash(password, salt);
+      const existing = await user.findOne({ email: email });
+  
+      if (existing !== null) {
+        res.status(200).json({
+          status: "failed",
+          message: "User already exists with this email, kindly login",
+        });
+      } else {
+        const saveData = await user.create({
+          email: email,
+          password: hashPassword,
+        });
+        res.status(201).json({
+          status: "success",
+          message: "You are registered successfully, please Log IN",
+        });
+      }
+    } catch (error) {
+      res.json({
+        status: "failed",
+        message: "Looks like some feilds are empty, please recheck",
+      });
+    }
+  });
+  
+  router.post("/login", async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const userdetails = await user.findOne({ email: email });
+      if (!userdetails) {
+        res.status(200).json({
+          status: "failed",
+          message: "Email does not exists kindly register first",
+        });
+      } else {
+        const isPasswordMatching = await bcrypt.compare(password, userdetails.password);
+        const token = jwt.sign({email:email,id:userdetails._id}, JWT_SECRET_KEY, {
+          expiresIn: "5d",
+        });
+        if (isPasswordMatching) {
+          res.status(200).json({
+            status: "success",
+            message: "Welcome!! authentication successful, you are logged in successfully",
+            jwt_token: token,
+            userid:userdetails._id
+          });
+        } else {
+          res.status(200).json({
+            status: "failed",
+            message: "Oops!! authentication failed, email or password is incorrect",
+          });
+        }
+      }
+      
+    } catch (error) {
+      res.status(404).json({
+        status: "failed",
+        message: "Kindly fill all the fields",
+      });
+    }
+  });
+  
+  //using middleware for authentication
+  
+  router.get("/user",async (req,res)=>{
+    const data = await user.find({email:req.user.email});
+      res.status(200).json({
+        status:"Success",
+        data:data
+      })
+    })
+  
+  module.exports = router;
+  
